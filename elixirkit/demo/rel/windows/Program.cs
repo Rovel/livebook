4b8f7410b03d5f1a03db611f4ac6768eab3532c4
Wojtek Mach <wojtek@wojtekmach.pl>
2022-12-16 12:15:33 +0100

    wip

--- elixirkit/demo/rel/windows/Program.cs
+++ elixirkit/demo/rel/windows/Program.cs
@@ -6,6 +6,59 @@ static class Program
     static void Main()
     {
         ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
+        Application.Run(new App());
+    }
+}
+
+// public class App : ApplicationContext
+public class App : Form
+{
+    private NotifyIcon trayIcon;
+
+    public App()
+    {
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.Text = "Demo";
+        Button button = new Button();
+        button.Location = new Point(50, 50);
+        button.Size = new Size(200, 80);
+        button.Text = "Press me!";
+        button.Click += this.HandleButtonClick;
+        this.Controls.Add(button);
+
+        ContextMenuStrip menu = new System.Windows.Forms.ContextMenuStrip();
+        menu.Items.Add("Exit", null, this.HandleExit);
+        String iconPath = System.Diagnostics.Process.GetCurrentProcess().MainModule!.FileName!;
+
+        trayIcon = new NotifyIcon()
+        {
+            Text = "Demo",
+            Visible = true,
+            Icon = Icon.ExtractAssociatedIcon(iconPath),
+            ContextMenuStrip = menu
+        };
+        trayIcon.Click += this.HandleIconClick;
+    }
+
+    void HandleIconClick(object? sender, EventArgs e)
+    {
+        MouseEventArgs mouseEventArgs = (MouseEventArgs)e;
+
+        if (mouseEventArgs.Button == MouseButtons.Left) {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+
+    void HandleExit(object? sender, EventArgs e)
+    {
+        trayIcon.Visible = false;
+        Application.Exit();
+    }
+
+    void HandleButtonClick(object? sender, EventArgs e)
+    {
+        Console.WriteLine("Button pressed!");
     }
 }
